7a646dfd8109998c2da8544a96232758cfa0e30e
raeeschachar <raees.chachar@arbisoft.com>
2018-03-22 01:05:04 +0500

    Added Dashboard page and product detail page

--- online_store/urls.py
+++ online_store/urls.py
@@ -1,8 +1,15 @@
+from django.conf import settings
 from django.conf.urls import url, include
 from django.contrib import admin
-
+from django.views.static import serve
 
 urlpatterns = [
     url(r'^admin/', admin.site.urls),
     url(r'^products/', include('products.urls')),
+    url(r'^dashboard/', include('dashboard.urls')),
 ]
+
+if settings.DEBUG:
+    urlpatterns += [
+        url(r'^media/(?P<path>.*)$', serve, {'document_root': settings.MEDIA_ROOT})
+    ]
--- products/views.py
+++ products/views.py
@@ -1,5 +1,16 @@
 from django.http import HttpResponse
+from django.shortcuts import render
+from django.views import View
+from models import Category, Product
 
 
 def index(request):
     return HttpResponse("Test view of Online Store Main")
+
+
+class ProductDetailView(View):
+    template_name = 'products/product_detail.html'
+
+    def get(self, request, product_id):
+        product = Product.objects.get(id=product_id)
+        return render(request, self.template_name, {'product': product})
